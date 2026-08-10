184f7bc3f4e67438f34aa9f5ab96353b98568978
Abhi2275 <moreabhishek2275@gmail.com>
2026-08-10 13:42:48 +0530

    fixed port issue

--- backend/server.js
+++ backend/server.js
@@ -1269,9 +1269,9 @@ app.get("/api/dashboard", (req, res) => {
 // START SERVER
 // =========================
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
-
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
 
